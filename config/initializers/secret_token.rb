614527c68bdd56bc9b5d9ca251c9cc197ddfcae3
K C <aries0420@gmail.com>
2013-09-19 16:36:19 -0700

    First commit for #6B.1 (authentication)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CfMusicgig::Application.config.secret_key_base = 'ea28b66905398696d84ea4a27225059e269162597543189daa93748376917dff9e027a49277af198bea5140c1b2843648990d70d2b982708390692b668085844'
