8f5ec47e473c4e9e5d0c5f408a841e3bae8d1f96
K C <aries0420@gmail.com>
2013-09-23 22:53:25 -0700

    Another modification on the initializer file

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -7,15 +7,12 @@ app.configure_with(:imagemagick)
 app.configure_with(:rails)
 
 if Rails.env.production?
-  app.datastore.configure do |c|
-    # Prohibit images from being accessed directly from S3 by the public.
-    # In conjunction with protect_from_dos_attacks (see below), this allows
-    # our rails app to enforce access, e.g. to allow access to a 64x64 thumbnail
-    # but prohibit access to the original image.
-    # c.storage_headers = {'x-amz-acl' => 'private'}
-    c.bucket_name = 'musicgig'
-    c.access_key_id = ENV['S3_KEY']
-    c.secret_access_key = ENV['S3_SECRET']
+  app.configure do |c|
+    c.datastore = Dragonfly::DataStorage::S3DataStore.new(
+      :bucket_name => 'musicgig',
+      :access_key_id => ENV['S3_KEY'],
+      :secret_access_key => ENV['S3_SECRET']
+    )
   end
 end
 
