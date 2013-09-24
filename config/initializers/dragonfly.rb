18ee5bde1a1ef9c4f0ccfa85c5c23a3d547b5c16
K C <aries0420@gmail.com>
2013-09-23 21:34:03 -0700

    adding S3 configuration

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -1,1 +1,20 @@
-require 'dragonfly/rails/images'
+# require 'dragonfly/rails/images'
+
+require 'dragonfly'
+
+app = Dragonfly[:images]
+app.configure_with(:imagemagick)
+app.configure_with(:rails)
+
+if Rails.env.production?
+  app.configure_with(:heroku, 'musicgig')
+  app.datastore.configure do |c|
+    # Prohibit images from being accessed directly from S3 by the public.
+    # In conjunction with protect_from_dos_attacks (see below), this allows
+    # our rails app to enforce access, e.g. to allow access to a 64x64 thumbnail
+    # but prohibit access to the original image.
+    c.storage_headers = {'x-amz-acl' => 'private'}
+  end
+end
+
+app.define_macro(ActiveRecord::Base, :image_accessor)
