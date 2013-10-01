f66a8e4cb255ff64a30638354cc968c732151ff2
K C <aries0420@gmail.com>
2013-10-01 15:36:07 -0700

    Added heroku link to navbar dropdown

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -1,21 +1,21 @@
-# require 'dragonfly/rails/images'
+require 'dragonfly/rails/images'
 
 # This is the config for S3
 
-require 'dragonfly'
+# require 'dragonfly'
 
-app = Dragonfly[:images]
-app.configure_with(:imagemagick)
-app.configure_with(:rails)
+# app = Dragonfly[:images]
+# app.configure_with(:imagemagick)
+# app.configure_with(:rails)
 
-if Rails.env.production?
-  app.configure do |c|
-    c.datastore = Dragonfly::DataStorage::S3DataStore.new(
-      :bucket_name => 'musicgig',
-      :access_key_id => ENV['S3_KEY'],
-      :secret_access_key => ENV['S3_SECRET']
-    )
-  end
-end
+# if Rails.env.production?
+#   app.configure do |c|
+#     c.datastore = Dragonfly::DataStorage::S3DataStore.new(
+#       :bucket_name => 'musicgig',
+#       :access_key_id => ENV['S3_KEY'],
+#       :secret_access_key => ENV['S3_SECRET']
+#     )
+#   end
+# end
 
-app.define_macro(ActiveRecord::Base, :image_accessor)
+# app.define_macro(ActiveRecord::Base, :image_accessor)
